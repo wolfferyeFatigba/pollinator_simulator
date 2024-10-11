4964e6555b5a4f04e56a8025a65070f2245ecbf9
wolfferyeFatigba <fatigbawolfferye@gmail.com>
2024-10-12 01:14:09 +0530

    drawing image 1st try

--- my_simulator.py
+++ my_simulator.py
@@ -3,10 +3,11 @@ pygame.init()
 width = 800
 height = 400
 screen = pygame.display.set_mode((width, height))
+bckgr = pygame.image.load('bckgr_simu2.jpeg')
+screen.blit(bckgr, (0, 0))
+pygame.display.flip()
 simulator = True
 while simulator :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             simulator = False
-
-pygame.display.flip()
